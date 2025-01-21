221897cdcf978032e5de7af08f11dc72fb1c2e78
DreamWeaver-Achiever <sakm2160@gmail.com>
2025-01-21 18:50:52 +0000

    Handling Web Requests

--- HandlingWebRequests/main.go
+++ HandlingWebRequests/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://www.example.com"
 
 func main() {
 	fmt.Println("Handling Web Requests in Golang")
+	resp, err := http.Get(url)
+	checkNilErr(err)
+	fmt.Printf("\nType of response: %T\n", resp)
+
+	defer resp.Body.Close() //callers responsiblity to close the request everytime.
+
+	dataBytes, err := ioutil.ReadAll(resp.Body)
+	checkNilErr(err)
+	content := string(dataBytes)
+	fmt.Printf("Contents received from web url/web request: %s", content)
+
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
