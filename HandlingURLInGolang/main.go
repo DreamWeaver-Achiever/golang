15afe6bc6aa8c6cfffad02719572455f15eb1819
DreamWeaver-Achiever <sakm2160@gmail.com>
2025-01-22 16:50:29 +0000

    Url operations

--- HandlingURLInGolang/main.go
+++ HandlingURLInGolang/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 )
+
+const urlPath = "https://www.example.com/path/to/resource?param1=value1"
 
 func main() {
 	fmt.Println("Handling Url In Golang")
+
+	exampleUrl, err := url.Parse(urlPath)
+	if err != nil {
+		panic(err)
+	}
+	
+	urlScheme := exampleUrl.Scheme
+	fmt.Println("Url Scheme: ", urlScheme)
+	
+	host := exampleUrl.Hostname()
+	fmt.Println("Host: ", host)
+
+	port := exampleUrl.Port()
+	fmt.Println("Port: ", port)
+
+	path := exampleUrl.Path 
+	fmt.Println("url Path: ", path)
+
+	rawQuery := exampleUrl.RawQuery
+	fmt.Println("rawQuery: ", rawQuery)
+
+	queryParam := exampleUrl.Query()
+	fmt.Println("Query parameter: ", queryParam)
+
+	query := exampleUrl.Query()
+	query.Set("param2", "value2")
+	exampleUrl.RawQuery = query.Encode()
+	fmt.Println("Modified url: ", exampleUrl.String())
+
 }
