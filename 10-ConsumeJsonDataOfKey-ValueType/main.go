f9cecaf4644513bcc1ae2a67a03a0d2e47e01868
DreamWeaver-Achiever <sakm2160@gmail.com>
2025-01-28 14:41:38 +0000

    Consume json data of key-value type

---
+++ 10-ConsumeJsonDataOfKey-ValueType/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Consume json data of key-value type")
+
+	jsonData := []byte(`{
+	"name": "John Doe",
+	"age": 42, 
+	"City": "New York"
+	}`)
+
+	//Check if json is valid
+	isJsonValid := json.Valid(jsonData)
+
+	//Decode json data
+	var decodedJson map[string]interface{}
+	if isJsonValid {
+		err := json.Unmarshal(jsonData, &decodedJson)
+		checkNilError(err, "Error while unmarshalling data.")
+	} else {
+		fmt.Println("Json is not valid.")
+	}
+	for k, v := range decodedJson {
+		fmt.Printf("\nJson Key: %v, Json Value: %v, Json value Type: %T", k, v, v);
+	}
+}
+
+func checkNilError(err error, errMsg string) {
+	if err != nil {
+		fmt.Println(errMsg)
+		return
+	}
+}
