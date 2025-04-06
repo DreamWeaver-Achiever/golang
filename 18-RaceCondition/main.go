fc83487af6ced99ea1a599f9475ac4f5592cd8d0
Sakshi M <sakm2160@gmail.com>
2025-04-06 22:47:52 +0000

    changing mutex positons

--- 18-RaceCondition/main.go
+++ 18-RaceCondition/main.go
@@ -13,23 +13,23 @@ func main() {
 	var score = []int{0}
 
 	wg.Add(3)
-	go func(wg *sync.WaitGroup, m *sync.Mutex) {
-		fmt.Println("First goroutine")
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
 		mut.Lock()
+		fmt.Println("First goroutine")
 		score = append(score, 1)
 		mut.Unlock()
 		wg.Done()
 	}(wg, mut)
-	go func(wg *sync.WaitGroup, m *sync.Mutex) {
-		fmt.Println("Second goroutine")
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
 		mut.Lock()
+		fmt.Println("Second goroutine")
 		score = append(score, 2)
 		mut.Unlock()
 		wg.Done()
 	}(wg, mut)
-	go func(wg *sync.WaitGroup, m *sync.Mutex) {
-		fmt.Println("Third goroutine")
+	go func(wg *sync.WaitGroup, mut *sync.Mutex) {
 		mut.Lock()
+		fmt.Println("Third goroutine")
 		score = append(score, 3)
 		mut.Unlock()
 		wg.Done()
