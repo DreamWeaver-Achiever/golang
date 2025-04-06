3e3c074ca692a2d31f97e613660e18dad772a927
Sakshi M <sakm2160@gmail.com>
2025-04-06 22:46:11 +0000

    Race Condition In Golang

--- 18-RaceCondition/main.go
+++ 18-RaceCondition/main.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
 	fmt.Println("Race Condition In Golang")
+
+	wg := &sync.WaitGroup{}
+	mut := &sync.Mutex{}
+	var score = []int{0}
+
+	wg.Add(3)
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+		fmt.Println("First goroutine")
+		mut.Lock()
+		score = append(score, 1)
+		mut.Unlock()
+		wg.Done()
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+		fmt.Println("Second goroutine")
+		mut.Lock()
+		score = append(score, 2)
+		mut.Unlock()
+		wg.Done()
+	}(wg, mut)
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+		fmt.Println("Third goroutine")
+		mut.Lock()
+		score = append(score, 3)
+		mut.Unlock()
+		wg.Done()
+	}(wg, mut)
+
+	wg.Wait()
+	fmt.Printf("Score: %d\n", score)
+
 }
