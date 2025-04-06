bea8c87c80f66da627222d71ec1bd0b57d1564c9
Sakshi M <sakm2160@gmail.com>
2025-04-06 21:57:45 +0000

    Mutex In Golang

---
+++ 17.Mutex/go.mod
@@ -0,0 +1,3 @@
+module 17.Mutex
+
+go 1.24.1
--- 17.Mutex/main.go
+++ 17.Mutex/main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"net/http"
 )
+
+var mut sync.Mutex
+var wg sync.WaitGroup
+var signals = []string{"test"}
 
 func main() {
 	fmt.Println("Mutex in Golang")
+	websiteList := []string {
+		"https://go.dev/", 
+		"https://google.com/", 
+		"https://github.com/", 
+	}
+	for _, web := range websiteList {
+		wg.Add(1)
+		go getStatusCode(web)
+	}
+	wg.Wait()
+	fmt.Printf("Signals after mutex: %s\n", signals)
+
+}
+
+func getStatusCode(website string) {
+	defer wg.Done()
+	
+	resp, err := http.Get(website)
+	if err != nil {
+		fmt.Printf("Error while fetching response from website: %s", website)
+	} else {
+		mut.Lock()
+		signals = append(signals, website)
+		defer mut.Unlock()
+		fmt.Printf("\nWebsite '%s' returned response: %d\n", website, resp.StatusCode)
+		resp.Body.Close() // Close the response body to prevent resource leaks.
+	}
 }
